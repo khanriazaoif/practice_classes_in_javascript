eb0dde4170418087965e6eb673137a170f2ed49a
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-22 17:20:01 -0600

    Unit 4 Practice Classes In JavaScript initial upload four complete

---
+++ four_practice_instantiating_an_object.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(email, username, birthday) {
+        this.email = email;
+        this.username = username;
+        this.birthday = birthday;
+    }
+}
+
+const user1 = new User("JavaScriptStudent@teamtreehouse.com", "JSUser1", "1/08/1991");
